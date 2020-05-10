3ce205bdc6bbcff5b162cc1edfcf6a5ebe11c709
Amar1729 <amar.paul16@gmail.com>
2020-05-09 21:22:26 -0400

    rtorrent: init at 0.9.8

---
+++ Formula/rtorrent.rb
@@ -0,0 +1,39 @@
+class Rtorrent < Formula
+  desc "Console-based BitTorrent client"
+  homepage "https://github.com/rakshasa/rtorrent"
+  url "http://rtorrent.net/downloads/rtorrent-0.9.8.tar.gz"
+  sha256 "9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cppunit" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "amar1729/rtorrent/libtorrent"
+  depends_on "xmlrpc-c" => :optional
+
+  # https://github.com/Homebrew/homebrew/issues/24132
+  fails_with :clang do
+    cause "Causes segfaults at startup/at random."
+  end
+
+  def install
+    # Commented out since we're now marked as failing with clang - adamv
+    # ENV.libstdcxx if ENV.compiler == :clang
+
+    args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
+    args << "--with-xmlrpc-c" if build.with? "xmlrpc-c"
+    if MacOS.version <= :leopard
+      inreplace "configure" do |s|
+        s.gsub! '  pkg_cv_libcurl_LIBS=`$PKG_CONFIG --libs "libcurl >= 7.15.4" 2>/dev/null`',
+          '  pkg_cv_libcurl_LIBS=`$PKG_CONFIG --libs "libcurl >= 7.15.4" | sed -e "s/-arch [^-]*/-arch $(uname -m) /" 2>/dev/null`'
+      end
+    end
+    system "sh", "autogen.sh"
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    doc.install "doc/rtorrent.rc"
+  end
+end
